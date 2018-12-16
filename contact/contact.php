9c0ff806e626644d5481da1930ea71c911240186
C.Rodriguez <crod@madebycrod.com>
2018-12-16 16:53:29 -0700

    update php

--- contact/contact.php
+++ contact/contact.php
@@ -8,11 +8,10 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 
 $mailTo = "contactform@skgiotservices.com";
-$headers = "From:".$firstname." ".$lastname;
 $headers = "From:".$email;
 $txt = "You have received and email from " .$firstname." ".$lastname.".\n\n".$message;
 
 
-mail($email, $subject, $txt, $headers);
+mail($mailTo, $subject, $txt, $headers);
 header("Location: index.php?mailsend");
 }

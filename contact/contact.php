61a5c24e258ac1b6158bfb25f0fc06ee9c22ad8c
C.Rodriguez <crod@madebycrod.com>
2018-12-17 07:40:30 -0700

    update contact page and what we do page

--- contact/contact.php
+++ contact/contact.php
@@ -1,17 +1,17 @@
 <?php
 
 if (isset($_POST['submit'])){
-$firstname = $_POST['first-name'];
-$lastname = $_POST['last-name'];
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
 $subject = $_POST['subject'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
 $mailTo = "contactform@skgiotservices.com";
 $headers = "From:".$email;
-$txt = "You have received and email from " .$firstname." ".$lastname.".\n\n".$message;
+$txt = "You have received and email from " .$firstname.".".$lastname.".\n\n".$message;
 
 
 mail($mailTo, $subject, $txt, $headers);
-header("Location: index.php?mailsend");
+header("Location: https://www.skgiotservices.com/");
 }
